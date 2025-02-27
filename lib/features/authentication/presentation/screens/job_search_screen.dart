402edb6af88cdde046d5c16cdc43d2c044edc745
mercyjae <famakinwamercy20@gmail.com>
2025-02-26 19:56:41 -0800

    feat: make chnages

--- lib/features/authentication/presentation/screens/job_search_screen.dart
+++ lib/features/authentication/presentation/screens/job_search_screen.dart
@@ -1,14 +1,11 @@
 import 'package:artisan_oga/core/app_constants/app_colors.dart';
 import 'package:artisan_oga/core/app_export.dart';
 import 'package:artisan_oga/core/utils/view_state.dart';
-import 'package:artisan_oga/features/authentication/domain/entities/category_response_entity.dart';
 import 'package:artisan_oga/features/authentication/domain/entities/search_job_data_entity.dart';
-import 'package:artisan_oga/features/authentication/domain/entities/skill_response_entity.dart';
 import 'package:artisan_oga/features/authentication/presentation/blocs/bloc/auth_bloc.dart';
 import 'package:artisan_oga/features/authentication/presentation/screens/j_s_create_account_page_one_screen.dart';
 import 'package:artisan_oga/features/authentication/presentation/screens/j_s_login_page_screen.dart';
-import 'package:artisan_oga/shared/widgets/custom_drop_down.dart';
-import 'package:artisan_oga/shared/widgets/custom_elevated_button.dart';
+import 'package:artisan_oga/features/authentication/presentation/widgets/job_seach_details.screen.dart';
 import 'package:artisan_oga/shared/widgets/custom_text_form_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -24,9 +21,9 @@ class JobSearchScreen extends HookWidget {
     final locationController = useTextEditingController();
     useEffect(() {
       context.read<AuthBloc>().add(AuthEvent.getCategory());
-      context.read<AuthBloc>().add(
-            AuthEvent.getSkills('1'),
-          );
+      // context.read<AuthBloc>().add(
+      //       AuthEvent.getSkills('1'),
+      //     );
 
       return null;
     }, []);
@@ -106,78 +103,86 @@ class JobSearchScreen extends HookWidget {
                   SizedBox(
                     height: 20.h,
                   ),
-                  CustomDropDown<CategoryResponseEntity>(
-                    title: 'Select Job Category',
-                    items: state.categoryList,
-                    selectedItem: state.categoryList.isNotEmpty
-                        ? state.categoryList.firstWhere(
-                            (category) =>
-                                category.id == (state.category?.id ?? 1),
-                            orElse: () => state.categoryList.first)
-                        : CategoryResponseEntity(id: 1, name: 'Fashion'),
-                    itemLabel: (category) => category.name,
-                    onChanged: (value) {
-                      context.read<AuthBloc>().add(
-                            AuthEvent.updateSelectedCategory(value!),
-                          );
+                  // GestureDetector(
+                  //   onTap: () {},
+                  //   child: CustomDropDown<CategoryResponseEntity>(
+                  //     title: 'Select Job Category',
+                  //     items: state.categoryList,
+                  //     selectedItem: state.categoryList.isNotEmpty
+                  //         ? state.categoryList.firstWhere(
+                  //             (category) =>
+                  //                 category.id == (state.category?.id ?? 1),
+                  //             orElse: () => state.categoryList.first)
+                  //         : CategoryResponseEntity(id: 1, name: 'Fashion'),
+                  //     itemLabel: (category) => category.name,
+                  //     onChanged: (value) {
+                  //       searchDialog(context, locationController);
+                  //       // context.read<AuthBloc>().add(
+                  //       //       AuthEvent.updateSelectedCategory(value!),
+                  //       //     );
 
-                      context.read<AuthBloc>().add(
-                            AuthEvent.getSkills(value.id.toString()),
-                          );
-                    },
-                  ),
-                  SizedBox(
-                    height: 20.h,
-                  ),
-                  CustomDropDown<SkillResponseEntity>(
-                    title: 'Select Job Skill',
-                    items: state.skill,
-                    selectedItem: state.skill.isNotEmpty
-                        ? state.skill.firstWhere(
-                            (category) =>
-                                category.id == (state.category?.id ?? 1),
-                            orElse: () => state.skill.first)
-                        : SkillResponseEntity(
-                            id: 1, name: 'Corset', categoryId: 1),
-                    itemLabel: (category) => category.name,
-                    onChanged: (value) {
-                      context.read<AuthBloc>().add(
-                            AuthEvent.updateSkill(value!),
-                          );
-                    },
-                  ),
-                  SizedBox(
-                    height: 20.h,
-                  ),
+                  //       // context.read<AuthBloc>().add(
+                  //       //       AuthEvent.getSkills(value.id.toString()),
+                  //       //     );
+                  //     },
+                  //   ),
+                  // ),
+                  // SizedBox(
+                  //   height: 20.h,
+                  // ),
+                  // CustomDropDown<SkillResponseEntity>(
+                  //   title: 'Select Job Skill',
+                  //   items: state.skill,
+                  //   selectedItem: state.skill.isNotEmpty
+                  //       ? state.skill.firstWhere(
+                  //           (category) =>
+                  //               category.id == (state.category?.id ?? 1),
+                  //           orElse: () => state.skill.first)
+                  //       : SkillResponseEntity(
+                  //           id: 1, name: 'Corset', categoryId: 1),
+                  //   itemLabel: (category) => category.name,
+                  //   onChanged: (value) {
+                  //     context.read<AuthBloc>().add(
+                  //           AuthEvent.updateSkill(value!),
+                  //         );
+                  //   },
+                  // ),
+                  // SizedBox(
+                  //   height: 20.h,
+                  // ),
                   CustomTextFormField(
+                    ontap: () {
+                      searchDialog(context, locationController);
+                    },
                     title: 'Location',
+                    readOnly: true,
                     controller: locationController,
                     hintText: "Enter location",
                     hintStyle: theme.textTheme.titleSmall!,
                   ),
-                  SizedBox(
-                    height: 20.h,
-                  ),
-                  Row(
-                    children: [
-                      CustomElevatedButton(
-                        width: 180.v,
-                        // isBusy: state.searchJobState == SearchJobState.loading,
-                        onPressed: () {
-                          context.read<AuthBloc>().add(
-                                AuthEvent.searchJobs(SearchJobDataEntity(
-                                    category: state.category?.id ?? 1,
-                                    location: locationController.text,
-                                    skill: state.singleSkill?.name ?? '')),
-                              );
-                        },
-                        text: "Search",
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 20.h,
-                  ),
+                  // SizedBox(
+                  //   height: 20.h,
+                  // ),
+                  // Row(
+                  //   children: [
+                  //     CustomElevatedButton(
+                  //       width: 180.v,
+                  //       // isBusy: state.searchJobState == SearchJobState.loading,
+                  //       onPressed: () {
+                  //         context.read<AuthBloc>().add(
+                  //               AuthEvent.searchJobs(SearchJobDataEntity(
+                  //                   category: state.category?.id ?? 1,
+                  //                   location: locationController.text,
+                  //                   skill: state.singleSkill?.name ?? '')),
+                  //             );
+                  //       },
+                  //       text: "Search",
+                  //     ),
+                  //   ],
+                  // ),
+                  // SizedBox(
+                  //   height: 20.h,
+                  // ),
                   state.searchJobState == SearchJobState.loading
                       ? Column(
                           children: [
@@ -191,9 +196,10 @@ class JobSearchScreen extends HookWidget {
                         )
                       : state.searchJobEntity.isEmpty
                           ? Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
                               children: [
                                 SizedBox(
-                                  height: 80,
+                                  height: 200,
                                 ),
                                 Text('No Result found',
                                     style: theme.textTheme.bodyMedium?.copyWith(
